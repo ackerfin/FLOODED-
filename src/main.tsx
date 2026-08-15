4cbd8cf06aa59dd068fd64b28c9f1227bb5a50e1
ackerfin <ackermanfin@gmail.com>
2026-08-15 22:35:22 +0700

    thêm eruda

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,7 @@ import { Buffer } from 'buffer';
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import eruda from 'eruda';
+eruda.init();
 
 createRoot(document.getElementById("root")!).render(<App />);
